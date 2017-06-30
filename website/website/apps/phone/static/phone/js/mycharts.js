d5d97e7ba1a5fa6db41d1cef452d6d45140aae6f
Tom Symonds <tdsymonds@hotmail.com>
2017-06-30 15:59:41 +0000

    Change line tooltip

--- website/website/apps/phone/static/phone/js/mycharts.js
+++ website/website/apps/phone/static/phone/js/mycharts.js
@@ -109,6 +109,14 @@
                     },
                     yAxis: {
                         min: 0
+                    },
+                    tooltip: {
+                        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                            '<td style="padding:0"><b>{point.y:.' + options.floatFormatDP + 'f}</b></td></tr>',
+                        footerFormat: '</table>',
+                        shared: true,
+                        useHTML: true
                     },                
                     series: series
                 };
